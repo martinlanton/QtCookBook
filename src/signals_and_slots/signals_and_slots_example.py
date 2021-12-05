43c688b7749159d35ab248e009481ca31d280e73
martinlanton <lantonmartin@gmail.com>
2021-12-04 23:57:41 -0800

    Started working with examples of signals and slots

---
+++ src/signals_and_slots/signals_and_slots_example.py
@@ -0,0 +1,16 @@
+from PySide6 import QtWidgets, QtCore, QtGui
+
+
+class Form(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        super(Form, self).__init__(parent)
+        dial = QtWidgets.QDial()
+        dial.setNotchesVisible(True)
+        spinbox = QtWidgets.QSpinBox()
+        layout = QtWidgets.QHBoxLayout()
+        layout.addWidget(dial)
+        layout.addWidget(spinbox)
+        self.setLayout(layout)
+        self.connect(dial, QtCore.SIGNAL("valueChanged(int)"), spinbox.setValue)
+        self.connect(spinbox, QtCore.SIGNAL("valueChanged(int)"), dial.setValue)
+        self.setWindowTitle("Signals and Slots")
