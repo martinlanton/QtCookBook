eb84bd49a5c0b4fd15966ed02f7a3d8be80fd212
martinlanton <lantonmartin@gmail.com>
2022-05-10 21:41:41 -0700

    Started converting ypipewidget.py

--- src/11_Custom_widgets/ypipewidget.py
+++ src/11_Custom_widgets/ypipewidget.py
@@ -10,63 +10,60 @@
 # the GNU General Public License for more details.
 
 
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
+from PySide6 import QtWidgets, QtCore, QtGui
 
 
-class YPipeWidget(QWidget):
-
-    def __init__(self, leftFlow=0, rightFlow=0, maxFlow=100,
-                 parent=None):
+class YPipeWidget(QtWidgets.QWidget):
+    def __init__(self, leftFlow=0, rightFlow=0, maxFlow=100, parent=None):
         super(YPipeWidget, self).__init__(parent)
 
-        self.leftSpinBox = QSpinBox(self)
+        self.leftSpinBox = QtWidgets.QSpinBox(self)
         self.leftSpinBox.setRange(0, maxFlow)
         self.leftSpinBox.setValue(leftFlow)
         self.leftSpinBox.setSuffix(" l/s")
-        self.leftSpinBox.setAlignment(Qt.AlignRight|Qt.AlignVCenter)
-        self.connect(self.leftSpinBox, SIGNAL("valueChanged(int)"),
-                     self.valueChanged)
+        self.leftSpinBox.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
+        self.connect(
+            self.leftSpinBox, QtCore.SIGNAL("valueChanged(int)"), self.valueChanged
+        )
 
-        self.rightSpinBox = QSpinBox(self)
+        self.rightSpinBox = QtWidgets.QSpinBox(self)
         self.rightSpinBox.setRange(0, maxFlow)
         self.rightSpinBox.setValue(rightFlow)
         self.rightSpinBox.setSuffix(" l/s")
-        self.rightSpinBox.setAlignment(Qt.AlignRight|Qt.AlignVCenter)
-        self.connect(self.rightSpinBox, SIGNAL("valueChanged(int)"),
-                     self.valueChanged)
+        self.rightSpinBox.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
+        self.connect(
+            self.rightSpinBox, QtCore.SIGNAL("valueChanged(int)"), self.valueChanged
+        )
 
-        self.label = QLabel(self)
-        self.label.setFrameStyle(QFrame.StyledPanel|QFrame.Sunken)
-        self.label.setAlignment(Qt.AlignCenter)
-        fm = QFontMetricsF(self.font())
-        self.label.setMinimumWidth(fm.width(" 999 l/s "))
+        self.label = QtWidgets.QLabel(self)
+        self.label.setFrameStyle(QtWidgets.QFrame.StyledPanel | QtWidgets.QFrame.Sunken)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        fm = QtGui.QFontMetricsF(self.font())
+        self.label.setMinimumWidth(fm.horizontalAdvance(" 999 l/s "))
 
-        self.setSizePolicy(QSizePolicy(QSizePolicy.Expanding,
-                                       QSizePolicy.Expanding))
+        self.setSizePolicy(
+            QtWidgets.QSizePolicy(
+                QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding,
+            )
+        )
         self.setMinimumSize(self.minimumSizeHint())
         self.valueChanged()
-
 
     def valueChanged(self):
         a = self.leftSpinBox.value()
         b = self.rightSpinBox.value()
         self.label.setText("{} l/s".format(a + b))
-        self.emit(SIGNAL("valueChanged"), a, b)
+        self.emit(QtCore.SIGNAL("valueChanged"), a, b)
         self.update()
-
 
     def values(self):
         return self.leftSpinBox.value(), self.rightSpinBox.value()
 
-
     def minimumSizeHint(self):
-        return QSize(self.leftSpinBox.width() * 3,
-                     self.leftSpinBox.height() * 5)
-
+        return QtCore.QSize(self.leftSpinBox.width() * 3, self.leftSpinBox.height() * 5)
 
     def resizeEvent(self, event=None):
-        fm = QFontMetricsF(self.font())
+        fm = QtGui.QFontMetricsF(self.font())
         x = (self.width() - self.label.width()) / 2
         y = self.height() - (fm.height() * 1.5)
         self.label.move(x, y)
@@ -76,73 +73,68 @@ class YPipeWidget(QWidget):
         x = self.width() - (self.width() / 4.0)
         self.rightSpinBox.move(x, y)
 
-
     def paintEvent(self, event=None):
         LogicalSize = 100.0
 
         def logicalFromPhysical(length, side):
             return (length / side) * LogicalSize
-        
-        fm = QFontMetricsF(self.font())
-        ymargin = ((LogicalSize / 30.0) +
-                   logicalFromPhysical(self.leftSpinBox.height(),
-                                       self.height()))
-        ymax = (LogicalSize -
-                logicalFromPhysical(fm.height() * 2, self.height()))
-        width = LogicalSize / 4.0
-        cx, cy = LogicalSize / 2.0, LogicalSize / 3.0
-        ax, ay = cx - (2 * width), ymargin
+
+        fm = QtGui.QFontMetricsF(self.font())
+        ymargin = (LogicalSize / 30.0) + logicalFromPhysical(
+            self.leftSpinBox.height(), self.height()
+        )
+        ymax = int(LogicalSize - logicalFromPhysical(fm.height() * 2, self.height()))
+        width = int(LogicalSize / 4.0)
+        cx, cy = int(LogicalSize / 2.0), int(LogicalSize / 3.0)
+        ax, ay = cx - int(2 * width), ymargin
         bx, by = cx - width, ay
         dx, dy = cx + width, ay
-        ex, ey = cx + (2 * width), ymargin
-        fx, fy = cx + (width / 2), cx + (LogicalSize / 24.0)
+        ex, ey = cx + int(2 * width), ymargin
+        fx, fy = cx + int(width / 2), cx + int(LogicalSize / 24.0)
         gx, gy = fx, ymax
-        hx, hy = cx - (width / 2), ymax
+        hx, hy = cx - int(width / 2), ymax
         ix, iy = hx, fy
 
-        painter = QPainter(self)
-        painter.setRenderHint(QPainter.Antialiasing)
+        painter = QtGui.QPainter(self)
+        painter.setRenderHint(QtGui.QPainter.Antialiasing)
         side = min(self.width(), self.height())
-        painter.setViewport((self.width() - side) / 2,
-                            (self.height() - side) / 2, side, side)
+        painter.setViewport(
+            (self.width() - side) / 2, (self.height() - side) / 2, side, side
+        )
         painter.setWindow(0, 0, LogicalSize, LogicalSize)
 
-        painter.setPen(Qt.NoPen)
+        painter.setPen(QtCore.Qt.NoPen)
 
-        gradient = QLinearGradient(QPointF(0, 0),
-                                         QPointF(0, 100))
-        gradient.setColorAt(0, Qt.white)
+        gradient = QtGui.QLinearGradient(QtCore.QPointF(0, 0), QtCore.QPointF(0, 100))
+        gradient.setColorAt(0, QtCore.Qt.white)
         a = self.leftSpinBox.value()
-        gradient.setColorAt(1, (Qt.red if a != 0 else Qt.white))
-        painter.setBrush(QBrush(gradient))
-        painter.drawPolygon(QPolygon([ax, ay, bx, by, cx, cy, ix, iy]))
+        gradient.setColorAt(1, (QtCore.Qt.red if a != 0 else QtCore.Qt.white))
+        painter.setBrush(QtGui.QBrush(gradient))
+        painter.drawPolygon(QtGui.QPolygon([QtCore.QPoint(ax, ay), QtCore.QPoint(bx, by), QtCore.QPoint(cx, cy), QtCore.QPoint(ix, iy)]))
 
-        gradient = QLinearGradient(QPointF(0, 0), QPointF(0, 100))
-        gradient.setColorAt(0, Qt.white)
+        gradient = QtGui.QLinearGradient(QtCore.QPointF(0, 0), QtCore.QPointF(0, 100))
+        gradient.setColorAt(0, QtCore.Qt.white)
         b = self.rightSpinBox.value()
-        gradient.setColorAt(1, (Qt.blue if b != 0
-                                else Qt.white))
-        painter.setBrush(QBrush(gradient))
-        painter.drawPolygon(QPolygon([cx, cy, dx, dy, ex, ey, fx, fy]))
+        gradient.setColorAt(1, (QtCore.Qt.blue if b != 0 else QtCore.Qt.white))
+        painter.setBrush(QtGui.QBrush(gradient))
+        painter.drawPolygon(QtGui.QPolygon([QtCore.QPoint(cx, cy), QtCore.QPoint(dx, dy), QtCore.QPoint(ex, ey), QtCore.QPoint(fx, fy)]))
 
         if (a + b) == 0:
-            color = QColor(Qt.white)
+            color = QtGui.QColor(QtCore.Qt.white)
         else:
-            ashare = (a / (a + b)) * 255.0
-            bshare = 255.0 - ashare
-            color = QColor(ashare, 0, bshare)
-        gradient = QLinearGradient(QPointF(0, 0), QPointF(0, 100))
-        gradient.setColorAt(0, Qt.white)
+            ashare = int((a / (a + b)) * 255.0)
+            bshare = int(255.0 - ashare)
+            color = QtGui.QColor(ashare, 0, bshare)
+        gradient = QtGui.QLinearGradient(QtCore.QPointF(0, 0), QtCore.QPointF(0, 100))
+        gradient.setColorAt(0, QtCore.Qt.white)
         gradient.setColorAt(1, color)
-        painter.setBrush(QBrush(gradient))
-        painter.drawPolygon(QPolygon(
-                [cx, cy, fx, fy, gx, gy, hx, hy, ix, iy]))
+        painter.setBrush(QtGui.QBrush(gradient))
+        painter.drawPolygon(QtGui.QPolygon([QtCore.QPoint(cx, cy), QtCore.QPoint(fx, fy), QtCore.QPoint(gx, gy), QtCore.QPoint(hx, hy), QtCore.QPoint(ix, iy)]))
 
-        painter.setPen(Qt.black)
-        painter.drawPolyline(QPolygon([ax, ay, ix, iy, hx, hy]))
-        painter.drawPolyline(QPolygon([gx, gy, fx, fy, ex, ey]))
-        painter.drawPolyline(QPolygon([bx, by, cx, cy, dx, dy]))
-
+        painter.setPen(QtCore.Qt.black)
+        painter.drawPolyline(QtGui.QPolygon([QtCore.QPoint(ax, ay), QtCore.QPoint(ix, iy), QtCore.QPoint(hx, hy)]))
+        painter.drawPolyline(QtGui.QPolygon([QtCore.QPoint(gx, gy), QtCore.QPoint(fx, fy), QtCore.QPoint(ex, ey)]))
+        painter.drawPolyline(QtGui.QPolygon([QtCore.QPoint(bx, by), QtCore.QPoint(cx, cy), QtCore.QPoint(dx, dy)]))
 
 
 if __name__ == "__main__":
@@ -151,12 +143,11 @@ if __name__ == "__main__":
     def valueChanged(a, b):
         print(a, b)
 
-    app = QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     form = YPipeWidget()
-    form.connect(form, SIGNAL("valueChanged"), valueChanged)
+    form.connect(form, QtCore.SIGNAL("valueChanged"), valueChanged)  # TODO : fix this signal connection
     form.setWindowTitle("YPipe")
     form.move(0, 0)
     form.show()
     form.resize(400, 400)
-    app.exec_()
-
+    app.exec()
